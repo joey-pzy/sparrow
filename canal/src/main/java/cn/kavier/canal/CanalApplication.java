cf15ecc76e4ccd447ee676e91b386f5783a6afb6
kavier <kavier@tom.com>
2022-07-02 18:18:53 +0800

    初始化demo项目

---
+++ canal/src/main/java/cn/kavier/canal/CanalApplication.java
@@ -0,0 +1,18 @@
+package cn.kavier.canal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(CanalApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+        log.info("=================canal项目 启动完成==================");
+    }
+
+}
---
+++ canal/src/test/java/cn/kavier/canal/CanalApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.kavier.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
